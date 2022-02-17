0e5861ac736db6b60cc6a1dffac538557f67bd2e
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-02-17 11:33:55 +0600

    successfully completed input function code

--- js/calculation.js
+++ js/calculation.js
@@ -1,56 +1,62 @@
 // Starting js 
 
-// function getInputValue(main){
-//     const mainBalanceText = document.getElementById('main-balance');
-//     const mainBalanceNum = mainBalanceText.value;
-//     const mainBalance = parseInt(mainBalanceNum);
-//     mainBalanceText.value = '';
-//     console.log(mainBalance);
-
-// }
+function getInputValue(inputId){
+    const inputBalanceText = document.getElementById(inputId);
+    const inputBalanceNum = inputBalanceText.value;
+    const inputBalance = parseInt(inputBalanceNum);
+    inputBalanceText.value = '';
+     return inputBalance;
+     
+}
 
 // Making claculate button clickable
 document.getElementById('calculate-btn').addEventListener('click', function(){
-    // const mainIncomeBalance = getInputValue()
+    const mainIncomeBalance = getInputValue('main-balance')
+    console.log(mainIncomeBalance);
 
     //Get Main Balane 
-     const mainBalanceText = document.getElementById('main-balance');
-     const mainBalanceNum = mainBalanceText.value;
-     const mainBalance = parseInt(mainBalanceNum);
-     mainBalanceText.value = '';
-     console.log(mainBalance);
+    //  const mainBalanceText = document.getElementById('main-balance');
+    //  const mainBalanceNum = mainBalanceText.value;
+    //  const mainBalance = parseInt(mainBalanceNum);
+    //  mainBalanceText.value = '';
+    //  console.log(mainBalance);
 
     //Get food fild value
-     const foodBillText = document.getElementById('food-bill');
-     const foodBillNum  = foodBillText.value;
-     const foodBill = parseInt(foodBillNum);   
-     foodBillText.value = '';
+    //  const foodBillText = document.getElementById('food-bill');
+    //  const foodBillNum  = foodBillText.value;
+    //  const foodBill = parseInt(foodBillNum);   
+    //  foodBillText.value = '';
     //  console.log(foodBill);
+    const foodCost = getInputValue('food-bill')
 
     //get rent fild value
 
-     const rentBillText = document.getElementById('rent-bill');
-     const rentBillNum  = rentBillText.value;
-     const rentBill = parseInt(rentBillNum);   
-     rentBillText.value = '';
+    //  const rentBillText = document.getElementById('rent-bill');
+    //  const rentBillNum  = rentBillText.value;
+    //  const rentBill = parseInt(rentBillNum);   
+    //  rentBillText.value = '';
     //  console.log(rentBill);
+
+    const rentCost = getInputValue('rent-bill')
 
     // get colthes fild value
 
-     const clothesBillText = document.getElementById('clothes-bill');
-     const clothesBillNum  = clothesBillText.value;
-     const clothesBill = parseInt(clothesBillNum);   
-     clothesBillText.value = '';
+    //  const clothesBillText = document.getElementById('clothes-bill');
+    //  const clothesBillNum  = clothesBillText.value;
+    //  const clothesBill = parseInt(clothesBillNum);   
+    //  clothesBillText.value = '';
     //  console.log(clothesBill);
+
+    const clothesCost = getInputValue('clothes-bill')
 
      const totalExpensestext = document.getElementById('total-expenses');
      const totalExpenses = parseInt (totalExpensestext.innerText);
-     const totalCost = foodBill + rentBill + clothesBill;
+     const totalCost = foodCost + rentCost + clothesCost;
      console.log(totalCost);
      totalExpensestext.innerText = totalCost;
 
     const balanceText = document.getElementById('balance');
-    const balance =  mainBalance - totalCost;
+    const balance =  mainIncomeBalance - totalCost;
     balanceText.innerText = balance;
     console.log(balance);
     // const balanceRemains = balance - totalExpenses;
@@ -59,9 +65,12 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
 })
 
 document.getElementById('saving-btn').addEventListener('click', function(){
-    const savingPercentText = document.getElementById('saving-percentage');
-    const savingPercent = parseFloat(savingPercentText.value);
-    // console.log(savingPercent);
+    // const savingPercentText = document.getElementById('saving-percentage');
+    // const savingPercent = parseFloat(savingPercentText.value);
+
+    const savingPercent = getInputValue('saving-percentage')
+
+
     const savingPercentAmount = savingPercent / 100;
     const savingAmmount = parseFloat(balance.innerText) * savingPercentAmount;
     console.log(savingAmmount);
